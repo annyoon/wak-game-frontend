c7cde37cdf77013e259a08d0c176a5fba67d7279
Seoan Yoon <earthdyoon@gmail.com>
2024-05-02 14:51:19 +0900

    feat: GrayBox 컴포넌트 개발

---
+++ src/components/GrayBox.tsx
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components';
+import { FlexLayout } from '../styles/layout';
+
+const StyledBox = styled(FlexLayout)<{ $width: string; $height: string }>`
+  width: ${(props) => props.$width};
+  height: ${(props) => props.$height};
+  border-style: solid;
+  border-left-width: 0rem;
+  border-right-width: 0rem;
+  border-top-width: 0.4rem;
+  border-bottom-width: 0.4rem;
+  background-color: #dadada;
+`;
+
+const BorderX = styled.img.attrs({
+  alt: '',
+})<{ $right?: boolean }>`
+  ${(props) =>
+    props.$right &&
+    css`
+      transform: rotate(0.5turn);
+    `};
+`;
+
+type GrayBoxProps = {
+  mode: 'SHORT' | 'TALL';
+  width: string;
+  children?: React.ReactNode;
+};
+
+export default function GrayBox({ mode, width, children }: GrayBoxProps) {
+  let img;
+  let height;
+
+  switch (mode) {
+    case 'SHORT':
+      img = require('../assets/borderImg/img-border-black-h196.png');
+      height = '18.8rem';
+      break;
+    case 'TALL':
+    default:
+      img = require('../assets/borderImg/img-border-black-h196.png');
+      height = '18.8rem';
+  }
+
+  return (
+    <FlexLayout>
+      <BorderX src={img} />
+      <StyledBox $isCol $width={width} $height={height}>
+        {children}
+      </StyledBox>
+      <BorderX src={img} $right />
+    </FlexLayout>
+  );
+}
