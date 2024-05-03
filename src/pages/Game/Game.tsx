336b29140e42fe36ef18657ff9a1301e7eb6652a
Seoan Yoon <earthdyoon@gmail.com>
2024-05-03 15:13:31 +0900

    feat: Game state 분리

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,26 +1,30 @@
+import { useState } from 'react';
 import { FlexLayout } from '../../styles/layout';
 
 import Background from '../../components/Background';
 import GameHeader from './components/GameHeader';
-import KillLog from './components/KillLog';
-import BattleField from './components/BattleField';
 import ChatBox from '../../components/ChatBox';
 import RankBox from './components/RankBox';
-import SpeechBubble from './components/SpeechBubble';
+
+import GameWait from './GameWait/GameWait';
+import GamePlay from './GamePlay/GamePlay';
+import GameResult from './GameResult/GameResult';
 
 export default function GamePage() {
+  const [state, setState] = useState<'WAIT' | 'PLAY' | 'RESULT'>('WAIT');
+
   return (
     <Background>
       <FlexLayout gap='4rem'>
         <FlexLayout $isCol gap='2rem'>
           <GameHeader />
-          <FlexLayout $isCol gap='1rem'>
-            <FlexLayout gap='2rem'>
-              <KillLog />
-              <SpeechBubble />
-            </FlexLayout>
-            <BattleField />
-          </FlexLayout>
+          {state === 'WAIT' ? (
+            <GameWait />
+          ) : state === 'PLAY' ? (
+            <GamePlay />
+          ) : (
+            <GameResult />
+          )}
         </FlexLayout>
         <FlexLayout $isCol gap='1.2rem'>
           <ChatBox isShort text={`방 채팅`} />
