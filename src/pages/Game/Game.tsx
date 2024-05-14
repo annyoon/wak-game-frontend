47f2504f668ddec8c56430db73cde7851cd39249
Seoan Yoon <earthdyoon@gmail.com>
2024-05-14 18:00:15 +0900

    feat: GameHeader 웹소켓 연결

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,6 +1,11 @@
-import { useState } from 'react';
-import { FlexLayout } from '../../styles/layout';
+import { useEffect, useRef, useState } from 'react';
+import SockJS from 'sockjs-client';
+import { CompatClient, Stomp } from '@stomp/stompjs';
 
+import { BASE_URL, getAccessToken } from '../../constants/api';
+import useGameStore from '../../store/gameStore';
+
+import { FlexLayout } from '../../styles/layout';
 import Background from '../../components/Background';
 import GameHeader from './components/GameHeader';
 import ChatBox from '../../components/ChatBox';
@@ -11,13 +16,46 @@ import GamePlay from './GamePlay/GamePlay';
 import GameResult from './GameResult/GameResult';
 
 export default function GamePage() {
+  const ACCESS_TOKEN = getAccessToken();
+  const header = {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+    'Content-Type': 'application/json',
+  };
+  const { gameData } = useGameStore();
   const [state, setState] = useState<'WAIT' | 'PLAY' | 'RESULT'>('WAIT');
+  const clientRef = useRef<CompatClient | null>(null);
+
+  const connectHandler = () => {
+    const socket = new SockJS(`${BASE_URL}/socket`);
+    clientRef.current = Stomp.over(socket);
+    clientRef.current.connect(header, () => {
+      clientRef.current?.subscribe(
+        `/topic/games/${gameData.roundId}/battle-field`,
+        (message) => {
+          // setPlayInfo(JSON.parse(message.body));
+        },
+        header
+      );
+    });
+  };
+
+  useEffect(() => {
+    connectHandler();
+    return () => {
+      clientRef.current?.disconnect(() => {
+        clientRef.current?.unsubscribe(
+          `/topic/games/${gameData.roundId}/battle-field`
+        );
+      }, header);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameData.roundId]);
 
   return (
     <Background>
       <FlexLayout gap='4rem'>
         <FlexLayout $isCol gap='2rem'>
-          <GameHeader />
+          <GameHeader clientRef={clientRef} />
           {state === 'WAIT' ? (
             <GameWait />
           ) : state === 'PLAY' ? (
--- src/pages/Game/components/GameHeader.tsx
+++ src/pages/Game/components/GameHeader.tsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from 'react';
+import { CompatClient } from '@stomp/stompjs';
+
+import { getAccessToken } from '../../../constants/api';
+import useGameStore from '../../../store/gameStore';
+
 import styled from 'styled-components';
 import { SmallText } from '../../../styles/fonts';
 
@@ -14,14 +20,47 @@ const TextBlock = styled.div`
   gap: 2rem;
 `;
 
-// type GameHeaderProps = {};
+type GameHeaderProps = {
+  clientRef: React.MutableRefObject<CompatClient | null>;
+};
 
-export default function GameHeader() {
+export default function GameHeader({ clientRef }: GameHeaderProps) {
+  const ACCESS_TOKEN = getAccessToken();
+  const header = {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+    'Content-Type': 'application/json',
+  };
+  const { gameData } = useGameStore();
+  const [info, setInfo] = useState({
+    roundNumber: 0,
+    totalCount: 0,
+    aliveCount: 0,
+  });
+
+  const subscription = () => {
+    clientRef.current?.subscribe(
+      `/topic/games/${gameData.roundId}/dashboard`,
+      (message) => {
+        setInfo(JSON.parse(message.body));
+      },
+      header
+    );
+  };
+
+  useEffect(() => {
+    subscription();
+    // return () =>
+    //   subscription?.unsubscribe(
+    //     `/topic/games/${gameData.roundId}/battle-field`
+    //   );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <HeaderBlock>
       <TextBlock>
-        <SmallText>{`현재 방 이름 : 덤벼 ( 1 라운드 )`}</SmallText>
-        <SmallText>{`생존자 수 : 40 / 40 명`}</SmallText>
+        <SmallText>{`현재 방 이름 : ${gameData.roomName} ( ${info.roundNumber} 라운드 )`}</SmallText>
+        <SmallText>{`생존자 수 : ${info.totalCount} / ${info.aliveCount} 명`}</SmallText>
         <SmallText>{`내 상태 : 생존 !`}</SmallText>
       </TextBlock>
     </HeaderBlock>
